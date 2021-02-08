a81276cac89ea6da0847770164d72f1e04950405
YaroslavC92 <78161548+YaroslavC92@users.noreply.github.com>
2021-02-08 09:26:06 -0500

    Add files via upload

---
+++ zadanie1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int var1, var2, sum;
+cin >> var1 >> var2;
+sum = var1 + var2;
+if((sum >= 10) && (sum <= 20)){
+cout << "true" << endl;
+}
+else{
+cout << "false";
+}
+    return 0;
+}
---
+++ zadanie3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int var1 = 5, var2 = 5;
+if((var1 == 10 && var2 == 10) || ((var1 + var2) == 10))
+    cout << "true";
+    return 0;
+}
